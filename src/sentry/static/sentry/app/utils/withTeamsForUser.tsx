8d863c3a51428c0022a35b459405d86ed4ff8b74
davidw1339 <davidw1339@gmail.com>
2019-10-29 00:56:08 -0700

    reuse previous project by team function

--- src/sentry/static/sentry/app/utils/withTeamsForUser.tsx
+++ src/sentry/static/sentry/app/utils/withTeamsForUser.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import _ from 'lodash';
 
 import {Client} from 'app/api';
-import {Organization, Project, Team} from 'app/types';
+import getProjectsByTeams from 'app/views/projectsDashboard/getProjectsByTeams';
+import {Organization, Team} from 'app/types';
 import getDisplayName from 'app/utils/getDisplayName';
-import ProjectActions from 'app/actions/projectActions';
 import ConfigStore from 'app/stores/configStore';
 import TeamActions from 'app/actions/teamActions';
 
@@ -45,7 +45,10 @@ const withTeamsForUser = <P extends InjectedTeamsProps>(
       let {teams} = this.props.organization;
       if (projects && teams) {
         const {isSuperuser} = ConfigStore.get('user');
-        this.populateTeamsWithProjects(teams, projects, isSuperuser);
+        const {projectsByTeam} = getProjectsByTeams(teams, projects, isSuperuser);
+        teams.forEach(team => {
+          team.projects = projectsByTeam[team.slug] || [];
+        });
         this.setState({
           teams,
           loadingTeams: false,
@@ -72,34 +75,6 @@ const withTeamsForUser = <P extends InjectedTeamsProps>(
           loadingTeams: false,
         });
       }
-    }
-
-    populateTeamsWithProjects(teams: Team[], projects: Project[], isSuperuser: boolean) {
-      const projectsByTeam = {};
-      let usersTeams = new Set(
-        teams.filter(team => team.isMember).map(team => team.slug)
-      );
-      if (usersTeams.size === 0 && isSuperuser) {
-        usersTeams = new Set(teams.map(team => team.slug));
-      }
-
-      projects.forEach(project => {
-        if (project.teams.length || project.isMember) {
-          project.teams.forEach(team => {
-            if (!usersTeams.has(team.slug)) {
-              return;
-            }
-            if (!projectsByTeam.hasOwnProperty(team.slug)) {
-              projectsByTeam[team.slug] = [];
-            }
-            projectsByTeam[team.slug].push(project);
-          });
-        }
-      });
-
-      teams.forEach(team => {
-        team.projects = projectsByTeam[team.slug] || [];
-      });
     }
 
     getUsersTeamsEndpoint() {
